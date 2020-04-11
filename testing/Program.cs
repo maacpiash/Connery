1771b7e4b4ca45701fd5ca4184f68c0b27adf50a
Ahad Chowdhury <ahadc@maacpiash.com>
2020-04-12 03:39:01 +1000

    Add testing project

---
+++ testing/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Connery.Lib;
+using System.Linq;
+
+namespace Connery.Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var images = ModelInput.LoadImagesFromDirectory(Paths.testImagesRelativePath, false);
+            Console.WriteLine("Actual label\tPredicted Label\tScore");
+            foreach (var image in images)
+            {
+                var prediction = ConsumeModel.Predict(image);
+                Console.WriteLine($"{image.Label}\t{prediction.Prediction}\t{prediction.Score.Max()}");
+            }
+        }
+    }
+}
