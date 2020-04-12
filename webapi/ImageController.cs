a251e11d03b8c137e25a23930a60a44dd284a737
Ahad Chowdhury <ahadc@maacpiash.com>
2020-04-12 22:37:22 +1000

    Add `ImageController` to Web API project
    The API controller is responsible for receiving images, classifying them
    and sending back the prediction (label + score). This controller has one
    POST method, which will do all these work.

---
+++ webapi/ImageController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Threading.Tasks;
+using Connery.Lib;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Connery.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ImageController : ControllerBase
+    {
+        private readonly ILogger<ImageController> logger;
+
+        public ImageController(ILogger<ImageController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<object> Post([FromBody]IFormFile image)
+        {
+            logger.LogInformation($"Request received at {new DateTime()}");
+            try
+            {
+                var tempFilePath = Path.GetTempFileName();
+                using (var ms = new FileStream(tempFilePath, FileMode.Create))
+                {
+                    await image.CopyToAsync(ms);
+                }
+                var prediction = ConsumeModel.Predict(new ModelInput { ImageSource = tempFilePath, Label = "anything" });
+                logger.LogInformation($"Predicted {prediction.Prediction} with a probablity of {prediction.Score.Max()}");
+                return prediction;
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e.ToString());
+                return e;
+            }
+        }
+    }
+}
