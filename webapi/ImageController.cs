b580fd9fa8f8214cd5480e4fde5f579979c6d65a
Ahad Chowdhury <ahadc@maacpiash.com>
2020-04-14 01:21:36 +1000

    Add coverage for edge cases
    In case of a request with no image or a non-image file, the server
    will behave accordingly from now on.

--- webapi/ImageController.cs
+++ webapi/ImageController.cs
@@ -22,25 +22,41 @@ namespace Connery.WebApi
         }
 
         [HttpPost]
-        public async Task<object> Post([FromForm]IFormFile image)
+        public async Task<object> Post(IFormFile image)
         {
             logger.LogInformation($"Request received at {DateTime.Now}");
+            if (image is null)
+            {
+                logger.LogInformation($"No image was found!");
+                return BadRequest("No image was found. Request body must be form-data in { \"image\": <file> } format");
+            }
+
             try
             {
                 var tempFilePath = Path.GetTempFileName();
                 using (var ms = new FileStream(tempFilePath, FileMode.Create))
                 {
                     await image.CopyToAsync(ms);
+                    logger.LogInformation($"{image.FileName} was saved to {tempFilePath}");
                 }
+
                 ModelInput input = new ModelInput { ImageSource = tempFilePath };
                 ModelOutput output = ConsumeModel.Predict(input, "MLModel.zip");
-                logger.LogInformation($"Predicted {output.Prediction} with a probablity of {output.Score.Max()}");
+
+                double probablity = output.Score.Max();
+                if (probablity == 0) // The file was probably not an image at all.
+                {
+                    logger.LogInformation("Error with the file!");
+                    return BadRequest("Error with the file!");
+                }
+
+                logger.LogInformation($"Predicted {output.Prediction} with a probablity of {probablity}.");
                 return new Response(output);
             }
             catch (Exception e)
             {
                 logger.LogError(e.ToString());
-                return e;
+                return BadRequest(e);
             }
         }
     }
