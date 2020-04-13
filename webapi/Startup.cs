082fec7b6326ffe013248da414f32fc0fc143918
Ahad Chowdhury <ahadc@maacpiash.com>
2020-04-14 02:28:37 +1000

    Add Swagger and Swagger UI
    The Swagger JSON object can be accessed at "swagger/v1/swagger.json".
    The Swagger UI can be accessed at "/" (home) path. The Swagger UI can be
    used to test any image directly.

--- webapi/Startup.cs
+++ webapi/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace Connery.WebApi
 {
@@ -27,11 +29,39 @@ namespace Connery.WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Connery Web API",
+                    Description = "A simple fruit-image-classifier",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Mohammad Abdul Ahad Chowdhury",
+                        Email = "ahadc@maacpiash.com",
+                        Url = new Uri("https://www.maacpiash.com"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "MIT",
+                        Url = new Uri("https://github.com/maacpiash/Connery/blob/master/LICENSE"),
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Connery Web API");
+                c.RoutePrefix = string.Empty;
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
